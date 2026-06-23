fa21c3f6c2c24122c4a5555c487957632b86a858
Trisha-Projects <jaintrisha146@gmail.com>
2026-06-23 13:58:28 +0530

    Converted sqlite3 to better-sqlite3

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import sqlite3 from "sqlite3";
+import Database from "better-sqlite3";
 import jwt from "jsonwebtoken";
 import winston from "winston";
 import bcrypt from "bcrypt";
@@ -50,26 +50,24 @@ app.use((req, res, next) => {
   next();
 });
 
-const db = new sqlite3.Database("./notes.db");
+const db = new Database("./notes.db");
 
 
-db.run(`
+db.exec(`
 CREATE TABLE IF NOT EXISTS notes(
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   title TEXT,
   content TEXT,
   user_id INTEGER,
   FOREIGN KEY(user_id) REFERENCES users(id)
-)
-`);
+);
 
-db.run(`
 CREATE TABLE IF NOT EXISTS users(
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   username TEXT UNIQUE,
- email TEXT UNIQUE,
+  email TEXT UNIQUE,
   password TEXT
-)
+);
 `);
 
 const notesSchema = Joi.object({
@@ -105,50 +103,14 @@ app.post("/login", (req, res) => {
 
   const { username, password } = req.body;
 
-  db.get(
-    "SELECT * FROM users WHERE username=?",
-    [username],
-
-    (err, user) => {
-
-      if (err) {
-        return res.status(500).json({
-          error: err.message,
-        });
-      }
-
-      if (!user) {
-        return res.status(401).json({
-          message: "Invalid Credentials",
-        });
-      }
-
-      const match = bcrypt.compareSync(
-        password,
-        user.password
-      ); // true or false
-
-      if (!match) {
-        return res.status(401).json({
-          message: "Invalid Credentials",
-        });
-      }
-
-const token = jwt.sign(
-  {
-    id: user.id,
-          username: user.username 
-        },
-  SECRET_KEY
-);
-      res.json({
-        token,
-      });4
-
-    }
-  );
-
+ const user = db
+  .prepare(
+    "SELECT * FROM users WHERE username=?"
+  )
+  .get(username);
 });
+
+
 
 // app.post("/send-mail", async (req, res) => {
 
@@ -217,41 +179,17 @@ app.post("/register", async (req, res) => {
     const hashedPassword =
       await bcrypt.hash(password, 10);
 
-    db.run(
-      "INSERT INTO users(username,email,password) VALUES (?,?,?)",
-      [username, email, hashedPassword],
+  db.prepare(
+  "INSERT INTO users(username,email,password) VALUES (?,?,?)"
+).run(username, email, hashedPassword);
 
-      function (err) {
-
-        if (err) {
-          console.log(err);
-
-          return res.status(500).json({
-            message: err.message,
-          });
-        }
-
-        console.log("abcd");
-          transporter.sendMail({
-
-      from: "jaintrisha146@gmail.com",
-      to: [email],
-      subject: "Welcome to KeepNote 📝",
-      text: `Hello ${username},
-            Your account has been created successfully.
-            Welcome to KeepNote! 📝`
-    });
-
-            res.json({
-          message: "Registration Successful",
-        });
-        
-      },
-      
+res.json({
+  message: "Registration Successful"
+});
        
 
 
-    );
+    
 
   } catch (err) {
 
@@ -266,14 +204,13 @@ app.post("/register", async (req, res) => {
 });
 
 app.get( "/api/notes", authenticateToken, (req, res) => {
-     db.all(
-  "SELECT * FROM notes WHERE user_id=?",
-  [req.user.id],
+   const rows = db
+  .prepare(
+    "SELECT * FROM notes WHERE user_id=?"
+  )
+  .all(req.user.id);
 
-      (err, rows) => {
-        res.json(rows);
-      }
-    );
+res.json(rows);
   }
 );
 
@@ -281,62 +218,38 @@ app.post("/api/notes", authenticateToken, (req, res) => {
 
   const { error } = notesSchema.validate(req.body);
       if (error) {
-        return res.status(400).json({ error: error.details[0].message });
+        return res.status(400).json({error: error.details[0].message});
       }
 
   const { title, content } = req.body;
 console.log("userid",req.user.id );
-  db.run(
-    "INSERT INTO notes(title,content,user_id) VALUES (?,?,?)",
-    [title, content, req.user.id],
 
+  const result = db
+  .prepare(
+    "INSERT INTO notes(title,content,user_id) VALUES (?,?,?)"
+  )
+  .run(title, content, req.user.id);
 
-    function (err) {
-
-      if (err) {
-        console.log(err);
-
-        return res.status(500).json({
-          message: err.message
-        });
-      }
-
-      res.json({
-        id: this.lastID,
-        title,
-        content
-      });
-
-    }
-
-  );
-
-});
+res.json({
+  id: result.lastInsertRowid,
+  title,
+  content
+});});
 
 app.put("/api/notes/:id", authenticateToken, (req, res) => {
 
   const { id } = req.params;
   const { title, content } = req.body;
 
-  db.run(
-    "UPDATE notes SET title=?, content=? WHERE id=? AND user_id=?",
-    [title, content, id, req.user.id],
+  db.prepare(
+  "UPDATE notes SET title=?, content=? WHERE id=? AND user_id=?"
+).run(title, content, id, req.user.id);
 
-    function(err) {
-
-      if (err) {
-        return res.status(500).json({
-          message: err.message
-        });
-      }
-      res.json({
-        id,
-        title,
-        content
-      });
-
-    }
-  );
+res.json({
+  id,
+  title,
+  content
+});
 
 });
 
@@ -344,24 +257,13 @@ app.delete("/api/notes/:id", authenticateToken, (req, res) => {
 
   const { id } = req.params;
 
-  db.run(
-    "DELETE FROM notes WHERE id=? AND user_id=?",
-    [id, req.user.id],
+  db.prepare(
+  "DELETE FROM notes WHERE id=? AND user_id=?"
+).run(id, req.user.id);
 
-    function(err) {
-
-      if (err) {
-        return res.status(500).json({
-          message: err.message
-        });
-      }
-
-      res.json({
-        message: "Deleted"
-      });
-
-    }
-  );
+res.json({
+  message: "Deleted"
+});
 
 });
 
